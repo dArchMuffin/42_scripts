31e0ae68b49bce23a5ab657f63ba65d387c78518
oelleaum <oelleaum@student.42lyon.fr>
2025-03-05 16:49:33 +0100

    tester fixed for args bash and added tests

--- pipex_tester/error_management.sh
+++ pipex_tester/error_management.sh
@@ -1,55 +1,150 @@
-error_management() {
+
+GREEN="\033[32m"
+RED="\033[31m"
+RESET="\033[0m"
+
+error_management()
+{
+  # Pierre : recuperer les variables du script parent ? 
   local TEST_NAME=$1
-  local FILE1_P=$2
+  local INFILE_P=$2
   local CMD1=$3
   local CMD2=$4
-  local FILE2_P=$5
-  local FILE1_B=$6
-  local FILE2_B=$7
+  local OUTFILE_P=$5
+  local B_CMD1=$6
+  local B_ARGS1=$7
+  local B_CMD2=$8
+  local B_ARGS2=$9
+  local INFILE_B=${10}
+  local OUTFILE_B=${11}
   local PIPEX_STDERR
   local BASH_STDERR
   local VALGRIND
   local LEAKS
   local CHECKED
-  local PIPEX_OUTPUT
-  local BASH_OUTPUT
+  local PIPEX_OUTPUT=${12}
+  local BASH_OUTPUT=${13}
 
-  ./pipex "$FILE1_P" "$CMD1" "$CMD2" "$FILE2_P" > "$PIPEX_OUTPUT_FILE" 2>&1
+  ./pipex "$INFILE_P" "$CMD1" "$CMD2" "$OUTFILE_P" > "$PIPEX_OUTPUT" 2>&1
   PIPEX_STDERR=$?
 
-  bash -c "< $FILE1_B $CMD1 | $CMD2 > $FILE2_B" > "$BASH_OUTPUT_FILE" 2>&1
+  bash -c "< $INFILE_B $B_CMD1 $B_ARGS1 | $B_CMD2 $B_ARGS2 > $OUTFILE_B" 2>/dev/null | sed 's/^bash : [^:]*: //' > "$BASH_OUTPUT" 2>&1
   BASH_STDERR=$?
 
-# + trace children=yes
-  VALGRIND=$(valgrind --leak-check=full --show-leak-kinds=all --track-fds=yes ./pipex "$FILE1_P" "$CMD1" "$CMD2" "$FILE2_P" 2>&1)
+  VALGRIND=$(valgrind --leak-check=full --show-leak-kinds=all --track-fds=yes ./pipex "$INFILE_P" "$CMD1" "$CMD2" "$OUTFILE_P" 2>&1)
   LEAKS=$(echo "$VALGRIND" | grep "All heap blocks were freed -- no leaks are possible" | wc -l)
   LEAKS=$((LEAKS + $(echo "$VALGRIND" | grep "ERROR SUMMARY: 0 errors from 0 contexts" | wc -l)))
   LEAKS=$((LEAKS + $(echo "$VALGRIND" | grep "FILE DESCRIPTORS: 3 open (3 std) at exit" | wc -l)))
 
-  if [[ $1 == "file1 doesn't exists" ]]; then
-    echo "" >> $FILE2_P
-  fi
+  chmod 755 $OUTFILE_P
+  chmod 755 $OUTFILE_B
 
-  CHECKED=$(diff "$FILE2_P" "$FILE2_B" | wc -l)
-  # echo "checked = $CHECKED"
-
-  if [[ $CHECKED -eq 0 && $LEAKS -eq 3 ]]; then
-    echo "OK : $TEST_NAME ./pipex $FILE1_P $CMD1 $CMD2 $FILE2_P | \$? = $PIPEX_STDERR"
-  elif [[ $CHECKED -ne 0 && $LEAKS -eq 3 ]]; then
-    echo "KO : ./pipex $FILE1_P $CMD1 $CMD2 $FILE2_P | \$? = $PIPEX_STDERR"
+  CHECKED=$(diff "$OUTFILE_P" "$OUTFILE_B" | wc -l 2>&1)
+  if [[ $CHECKED -eq 0 && $LEAKS -ge 3 ]]; then
+    echo -e "$GREEN OK$RESET: $TEST_NAME"
+  elif [[ $CHECKED -ne 0 && $LEAKS -ge 3 ]]; then
+    echo "KO :$TEST_NAME : ./pipex $INFILE_P $CMD1 $CMD2 $OUTFILE_P > $PIPEX_OUTPUT | \$? = $PIPEX_STDERR"
     local LOG=$LOG_FOLDER/test_$(date +%s).log
     touch "$LOG"
     {
-      echo "PROMPT : ./pipex $FILE1_P $CMD1 $CMD2 $FILE2_P"
-      echo "pipex output : $(cat "$PIPEX_OUTPUT_FILE")"
+      echo "$TEST_NAME : ./pipex $INFILE_P $CMD1 $CMD2 $OUTFILE_P > $PIPEX_OUTPUT"
+      echo ""
+      echo "pipex output : $(cat "$PIPEX_OUTPUT")"
+      echo "bash output : $(cat "$BASH_OUTPUT")"
+      echo ""
       echo "pipex infile : $(cat "$FILE1_P" 2>&1)"
-      echo "pipex outfile : $(cat "$FILE2_P")"
-      echo "bash output : $(cat "$BASH_OUTPUT_FILE")"
       echo "bash infile : $(cat "$FILE1_B" 2>&1)"
+      echo ""
+      echo "pipex outfile : $(cat "$FILE2_P")"
       echo "bash outfile : $(cat "$FILE2_B")"
       echo "---------------------------------------------------------------------------------"
     } >> "$LOG"
-  elif [[ $LEAKS -ne 3 ]]; then
+  elif [[ $LEAKS -lt 3 ]]; then
     echo "LEAKS"
+    echo -e "$TEST_NAME : ./pipex $INFILE_P $CMD1 $CMD2 $OUTFILE_P > $PIPEX_OUTPUT"
+    echo "$VALGRIND" >> "vlog"
+ # mettre les logs de valgrind dans logs 
+    echo -e "\n---------------------------------------------------------------------------------\n" >> "vlog"
   fi
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+#
+#
+# error_management() {
+#   local TEST_NAME=$1
+#   local FILE1_P=$2
+#   local CMD1=$3
+#   local CMD2=$4
+#   local FILE2_P=$5
+#   local FILE1_B=$6
+#   local FILE2_B=$7
+#   local PIPEX_STDERR
+#   local BASH_STDERR
+#   local VALGRIND
+#   local LEAKS
+#   local CHECKED
+#   local PIPEX_OUTPUT=$8
+#   local BASH_OUTPUT=$9
+#
+#   ./pipex "$FILE1_P" "$CMD1" "$CMD2" "$FILE2_P" > "$PIPEX_OUTPUT" 2>&1
+#   PIPEX_STDERR=$?
+#
+#   # sed a virer ? 
+#   bash -c "< $FILE1_B $CMD1 | $CMD2 > $FILE2_B" 2>/dev/null | sed 's/^bash : [^:]*: //' > "$BASH_OUTPUT" 2>&1
+#   BASH_STDERR=$?
+#
+#   VALGRIND=$(valgrind --leak-check=full --show-leak-kinds=all --track-fds=yes ./pipex "$FILE1_P" "$CMD1" "$CMD2" "$FILE2_P" 2>&1)
+#   LEAKS=$(echo "$VALGRIND" | grep "All heap blocks were freed -- no leaks are possible" | wc -l)
+#   LEAKS=$((LEAKS + $(echo "$VALGRIND" | grep "ERROR SUMMARY: 0 errors from 0 contexts" | wc -l)))
+#   LEAKS=$((LEAKS + $(echo "$VALGRIND" | grep "FILE DESCRIPTORS: 3 open (3 std) at exit" | wc -l)))
+#
+#   chmod 755 $FILE2_P
+#   chmod 755 $FILE2_B
+#
+#   CHECKED=$(diff "$FILE2_P" "$FILE2_B" | wc -l 2>&1)
+#   # echo "checked = $CHECKED"
+#
+#   if [[ $CHECKED -eq 0 && $LEAKS -ge 3 ]]; then
+#     echo "OK :$TEST_NAME : ./pipex $FILE1_P $CMD1 $CMD2 $FILE2_P | \$? = $PIPEX_STDERR"
+#   elif [[ $CHECKED -ne 0 && $LEAKS -ge 3 ]]; then
+#     echo "KO :$TEST_NAME : ./pipex $FILE1_P $CMD1 $CMD2 $FILE2_P | \$? = $PIPEX_STDERR"
+#     local LOG=$LOG_FOLDER/test_$(date +%s).log
+#     touch "$LOG"
+#     {
+#       echo "$TEST_NAME : ./pipex $FILE1_P $CMD1 $CMD2 $FILE2_P"
+#       echo ""
+#       echo "pipex output : $(cat "$PIPEX_OUTPUT")"
+#       echo "bash output : $(cat "$BASH_OUTPUT")"
+#       echo ""
+#       echo "pipex infile : $(cat "$FILE1_P" 2>&1)"
+#       echo "bash infile : $(cat "$FILE1_B" 2>&1)"
+#       echo ""
+#       echo "pipex outfile : $(cat "$FILE2_P")"
+#       echo "bash outfile : $(cat "$FILE2_B")"
+#       echo "---------------------------------------------------------------------------------"
+#     } >> "$LOG"
+#   elif [[ $LEAKS -lt 3 ]]; then
+#     echo "LEAKS"
+#     echo -e "$TEST_NAME : ./pipex $FILE1_P $CMD1 $CMD2 $FILE2_P\n" >> "vlog"
+#     echo "$VALGRIND" >> "vlog"
+#  # mettre les logs de valgrind dans logs 
+#     echo -e "\n---------------------------------------------------------------------------------\n" >> "vlog"
+#   fi
+# }
--- pipex_tester/pipex_tester.sh
+++ pipex_tester/pipex_tester.sh
@@ -13,50 +13,74 @@ fi
 
 FOLDER=pipex_tester_tmp
 LOG_FOLDER=log
+BASH_FOLDER=$FOLDER/bash
+PIPEX_FOLDER=$FOLDER/pipex
+PIPEX_INFILE=$PIPEX_FOLDER/infile
+BASH_INFILE=$BASH_FOLDER/infile
+PIPEX_OUTFILE=$PIPEX_FOLDER/outfile
+BASH_OUTFILE=$BASH_FOLDER/outfile
 
 mkdir -p $FOLDER
+mkdir -p $BASH_FOLDER
+mkdir -p $PIPEX_FOLDER
 mkdir -p $LOG_FOLDER
 
-PIPEX_FILE1=$FOLDER/pipex_infile
-PIPEX_FILE2=$FOLDER/pipex_outfile
-PIPEX_OUTPUT_FILE=$FOLDER/pipex_output
-BASH_FILE1=$FOLDER/bash_infile
-BASH_FILE2=$FOLDER/bash_outfile
-BASH_OUTPUT_FILE=$FOLDER/bash_output
+PIPEX_OUTPUT_FILE=$PIPEX_FOLDER/output
+BASH_OUTPUT_FILE=$BASH_FOLDER/output
 
-touch $PIPEX_FILE1
-touch $PIPEX_FILE2
-touch $BASH_FILE1
-touch $BASH_FILE2
+touch $PIPEX_INFILE
+touch $BASH_INFILE
+touch $PIPEX_OUTFILE
+touch $BASH_OUTFILE
 touch $PIPEX_OUTPUT_FILE
 touch $BASH_OUTPUT_FILE
 
-echo "This is a test file." > $PIPEX_FILE1
-echo "This is a test file." > $BASH_FILE1
+echo "This is a test file." > $PIPEX_OUTFILE
+echo "This is a test file." > $BASH_OUTFILE
 
 # Error_management tests
 echo "--- Error_management tests ---"
-touch $PIPEX_FILE2
-touch $BASH_FILE2
-rm $PIPEX_FILE1
-rm $BASH_FILE1
-error_management "file1 doesn't exists" "$PIPEX_FILE1" "ls" "echo" "$PIPEX_FILE2" "$BASH_FILE1" "$BASH_FILE2"
-echo "" > pipex_tester_tmp/pipex_outfile
-echo "" > pipex_tester_tmp/bash_outfile
-touch $PIPEX_FILE1 
-touch $BASH_FILE1 
-chmod 000 $PIPEX_FILE1
-chmod 000 $BASH_FILE1
-error_management "file1 has no permissions" "$PIPEX_FILE1" "ls" "echo" "$PIPEX_FILE2" "$BASH_FILE1" "$BASH_FILE2"
-# a completer 
-chmod 644 "$PIPEX_FILE1"
-chmod 644 "$BASH_FILE1"
-#Kenzo cracked : env -i ./pipex 
-# + PATH vide PATH= (zsh : PATH="")
+touch $PIPEX_INFILE
+touch $BASH_INFILE
+rm $PIPEX_INFILE
+rm $BASH_INFILE
+error_management "infile doesn't exists" "$PIPEX_INFILE" "ls -l" "wc -l" "$PIPEX_OUTFILE" "ls" "-l" "wc" "-l" "$BASH_INFILE" "$BASH_OUTFILE" "$PIPEX_OUTPUT_FILE" "$BASH_OUTPUT_FILE"
+touch $PIPEX_INFILE 
+touch $BASH_INFILE 
+chmod 000 $PIPEX_INFILE
+chmod 000 $BASH_INFILE
+error_management "infile has no permissions" "$PIPEX_INFILE" "ls -l" "wc -l" "$PIPEX_OUTFILE" "ls" "-l" "wc" "-l" "$BASH_INFILE" "$BASH_OUTFILE" "$PIPEX_OUTPUT_FILE" "$BASH_OUTPUT_FILE"
+chmod 755 "$PIPEX_INFILE"
+chmod 755 "$BASH_INFILE"
+rm -rf $PIPEX_OUTFILE
+rm -rf $BASH_OUTFILE
+error_management "outfile doesn't exists" "$PIPEX_INFILE" "ls -l" "wc -l" "$PIPEX_OUTFILE" "ls" "-l" "wc" "-l" "$BASH_INFILE" "$BASH_OUTFILE" "$PIPEX_OUTPUT_FILE" "$BASH_OUTPUT_FILE"
+touch $PIPEX_OUTFILE 
+touch $BASH_OUTFILE 
+chmod 000 $PIPEX_OUTFILE
+chmod 000 $BASH_OUTFILE
+error_management "outfile has no permissions" "$PIPEX_INFILE" "ls -l" "wc -l" "$PIPEX_OUTFILE" "ls" "-l" "wc" "-l" "$BASH_INFILE" "$BASH_OUTFILE" "$PIPEX_OUTPUT_FILE" "$BASH_OUTPUT_FILE"
+chmod 755 "$PIPEX_OUTFILE"
+chmod 755 "$BASH_OUTFILE"
+error_management "incorrect cmd1" "$PIPEX_INFILE" "lsg -l" "wc -l" "$PIPEX_OUTFILE" "lsg" "-l" "wc" "-l" "$BASH_INFILE" "$BASH_OUTFILE" "$PIPEX_OUTPUT_FILE" "$BASH_OUTPUT_FILE"
+error_management "incorrect cmd2" "$PIPEX_INFILE" "ls -l" "wcg -l" "$PIPEX_OUTFILE" "ls" "-l" "wcg" "-l" "$BASH_INFILE" "$BASH_OUTFILE" "$PIPEX_OUTPUT_FILE" "$BASH_OUTPUT_FILE"
+# ce test est bon ?
+error_management "/dev/urandom as infile" "/dev/urandom" "cat" "sleep 3" "$PIPEX_OUTFILE" "cat" "" "sleep" "3" "/dev/urandom" "$BASH_OUTFILE" "$PIPEX_OUTPUT_FILE" "$BASH_OUTPUT_FILE"
+error_management "sleep 5 | sleep 5" "$PIPEX_INFILE" "sleep 5" "sleep 5" "$PIPEX_OUTFILE" "sleep" "5" "sleep" "5" "$PIPEX_OUTFILE" "$BASH_OUTFILE" "$PIPEX_OUTPUT_FILE" "$BASH_OUTPUT_FILE"
 
-error_management "without env" ""
+
+# rm -rf $FOLDER
+#
+# /dev/urandom en infile / outfile
+# sleep 5 | sleep 5
+# infile cat | cat | ls outfile
 
 # Une fois le pipex fonctionnel :
 # file_output "$PIPEX_FILE1" "ls -l" "wc -l" "$PIPEX_FILE2" "$BASH_FILE1" "$BASH_FILE2"
 
-rm -rf $FOLDER
+# A faire a la main ? 
+# < infile ls -l -a -r -t | wc -l -rgergr > outfile
+#./pipex /dev/urandom "cat" "head -2" outfile
+
+# env -i : NON ?
+# /bin/ls comme cmd : NON
